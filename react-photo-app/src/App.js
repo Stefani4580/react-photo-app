81241963abfd687040c86226d20bb853b89dd91e
Stefani Baker <stefanibaker@MacBook-Air-4.local>
2020-07-24 16:11:12 -0400

    Added React-Bootstrap dependencies.  Added Navbar

--- react-photo-app/src/App.js
+++ react-photo-app/src/App.js
@@ -1,14 +1,50 @@
-import React from 'react';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
 class App extends React.Component {
+  constructor() {
+    super();
+  }
+
   render() {
-  return (
-    <div className="App">
-      React Photo App
-    </div>
-  );
+    return (
+      <div className="App">
+        <Container fluid>
+          <Navbar bg="light" expand="lg">
+            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#link">Link</Nav.Link>
+                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">
+                    Another action
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">
+                    Something
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">
+                    Separated link
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+          <Row>
+            <Col>1 of 1</Col>
+          </Row>
+        </Container>
+      </div>
+    );
   }
 }
 
