1d3a787a52bfb1662152dcefeddab3a91a2f53d0
Stefani Baker <stefanibaker@MacBook-Air-4.local>
2020-07-25 11:07:40 -0400

    Added user input form in modal

--- react-photo-app/src/components/UserLogin.js
+++ react-photo-app/src/components/UserLogin.js
@@ -1,12 +1,20 @@
 import React from "react";
+
+// Bootstrap
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
+import Form from 'react-bootstrap/Form';
 
 function UserLogin() {
   const [show, setShow] = React.useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form submitted");
+    handleClose();
+  }
 
   return (
     <div>
@@ -18,13 +26,32 @@ function UserLogin() {
         <Modal.Header closeButton>
           <Modal.Title>Modal heading</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Body>
+          <Form>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group controlId="formBasicCheckbox">
+              <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
         <Modal.Footer>
+        <Button variant="primary" type="submit" onClick={handleSubmit}>
+              Submit
+            </Button>
+
           <Button variant="secondary" onClick={handleClose}>
             Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
           </Button>
         </Modal.Footer>
       </Modal>
