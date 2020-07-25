234488008a5245f21cc80fb4ec924ca63eae8e90
Stefani Baker <stefanibaker@MacBook-Air-4.local>
2020-07-25 10:30:56 -0400

    Copied assets to components folder.  Error if not there.  Strange.

--- react-photo-app/src/components/UserProfile.js
+++ react-photo-app/src/components/UserProfile.js
@@ -3,15 +3,34 @@ import React from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Image from "react-bootstrap/Image";
+import pic from "../assets/images/alex-nemo-hanse-_KP6Ve-rnNw-unsplash.jpg"
+// Helper Classes and Functions
+// import imageLoader from "./images";
+// import MyImage from "./MyImage";
+
 
 const UserProfile = (props) => {
 
+
+//  const loadImages = () => {
+//     let jpgs = imageLoader();
+//     let images = [];
+//     for (let i = 0; i < jpgs.length; i++) {
+//       let image = new MyImage(jpgs[i].id, jpgs[i].src);
+//       images[i] = image;
+//     }
+//     return images;
+//   };
+
+
     //Simulating Users Photos - just post a few by using %.  Post the index is divisible by 3.
   const listOfImages = props.images.map((item, index) => {
+    let filename = `.${item.filename}`;
+        console.log(`filename: ${filename}`);    
     if (index % 3 === 0) {
       return (
         <Col xs={6} md={4} lg={3}>
-          <Image className="image" src={`.${item.filename}`} rounded />
+          <Image className="image" src={require(`${item.filename}`)} rounded />
         </Col>
       );
     } else {
